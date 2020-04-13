92166e221cca8d8feb9fd782db5e58c2d0ab6668
Arber Shabani <arber.shabani@hotmail.com>
2020-04-13 12:53:07 +0200

    Add Header

---
+++ components/classes/extras/resources/resource.extra.js
@@ -0,0 +1,36 @@
+const createFile = require("../../../../services/createFile.service");
+const createStyleFile = require("../../../../services/createStyleFile.service");
+const checkFolder = require("../../../../services/folder.service");
+const classResourceComponent = require("../../class");
+const createResourceComponent = require("./resources/create");
+const deleteResourceComponent = require("./resources/delete");
+const editResourceComponent = require("./resources/edit");
+const camelToKebab = require("../../../../services/camelToKebab.service");
+
+module.exports = async (name, fields) => {
+	// checkFolder.components(name);
+
+	// await createFile(`src/components/${name}.js`, content(name, state, componentDidMount, componentDidUpdate));
+	// console.log("Class Component Created");
+
+	let Name = name.split("/").pop().capitalize();
+	if (!Name.endsWith("s")) Name = Name + "s";
+	const folderName = name
+		.split("/")
+		.map((_name) => camelToKebab(_name.toLowerFirst()))
+		.join("/");
+	const nameSingle = Name.slice(0, -1);
+
+	const allFields = fields.split(",").map((field) => field.trim());
+
+	// await createResourceComponent(`${folderName}/${Name}`, Name, state, componentDidMount, componentDidUpdate);
+	await classResourceComponent(`${folderName}/_Header`);
+	// await createResourceComponent(`${folderName}/${nameSingle}`);
+	await createResourceComponent(`${folderName}/resources/Create${nameSingle}`, `Create${nameSingle}`, allFields);
+	await deleteResourceComponent(`${folderName}/resources/Delete${nameSingle}`, `Delete${nameSingle}`, allFields);
+	await editResourceComponent(`${folderName}/resources/Edit${nameSingle}`, `Edit${nameSingle}`, allFields);
+	// await createResourceComponent(`${folderName}/resources/List${Name}`);
+	// await createResourceComponent(`${folderName}/resources/Show${nameSingle}`);
+
+	process.exit();
+};
